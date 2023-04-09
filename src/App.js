66681de2de1604e1062c3d9b600a532dcaf8d76a
devshaah <devshah0404@gmail.com>
2023-04-09 06:51:28 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const[input,set] = useState("");
+  const[input2,set2] = useState("0");
+
+  const addf = (val) => 
+  {
+    set2(" ");
+    set(input + val);
+  }
+
+  const clearrall = () => {
+    set2("0")
+    set(" ")
+  }
+
+  const clearlast = () => {
+    const a = input.lastIndexOf()
+    set(input.slice(0,a))
+  }
+
+  let answer = () => {
+try{  
+  let ans = eval(input);
+  set(ans);
+  }
+catch{
+  set("Invalid")
+}}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="container">
+      <div  class="output screen">      
+        {input2}{input}
+      </div>
+      <div>
+      <button type="button" class="btn btn-outline-dark red" onClick={clearrall}>CLR</button>
+      <button type="button" class="btn btn-outline-dark yellow" onClick={clearlast}>AC</button>
+      <button type="button" class="btn btn-outline-dark yellow" onClick={()=>addf('%')}>%</button>
+      <button type="button" class="btn btn-outline-dark yellow" onClick={()=>addf('/')}>/</button>
+      </div>
+      <div>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(7)}>7</button>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(8)}>8</button>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(9)}>9</button>
+      <button type="button" class="btn btn-outline-dark yellow" onClick={()=>addf('*')}>X</button>
+      </div>
+      <div>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(4)}>4</button>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(5)}>5</button>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(6)}>6</button>
+      <button type="button" class="btn btn-outline-dark yellow" onClick={()=>addf('-')}>-</button>
+      </div>
+      <div>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(1)}>1</button>
+      <button type="button" class="btn btn-outline-dark"  onClick={()=>addf(2)}>2</button>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(3)}>3</button>
+      <button type="button" class="btn btn-outline-dark yellow" onClick={()=>addf('+')}>+</button>
+      </div>
+      <div>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(0)}>0</button>
+      <button type="button" class="btn btn-outline-dark" onClick={()=>addf(".")}>.</button>
+      <button type="button" class="btn btn-outline-dark equal" onClick={answer} >=</button>
+      </div>
+
     </div>
   );
 }
